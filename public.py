2abaae1131c284431e2a7c0f045501a26353c106
hasiddiqui2 <Hammad.Siddiqui32@myhunter.cuny.edu>
2023-05-04 14:04:07 -0400

    updated the code to handle multiple ids at once and create one combined comment list for all of them

--- public.py
+++ public.py
@@ -47,18 +47,27 @@ from utils.comments import process_comments, make_csv
 API_KEY = "AIzaSyA5pIGUOIuLyFgf2BK7MCyiOFXmIUg9Gl8"
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
+def video_threads(videoIDs):
+   comments_list = []
+   for videoID in videoIDs:
+      request = youtube.commentThreads().list(
+         part='id, snippet',
+         videoId=videoID,
+         maxResults = 100
+         )
+      response = request.execute()
+      comments_list.extend(process_comments(response['items']))
+   return comments_list
+
 def comment_threads(videoID):
    comments_list = []
    request = youtube.commentThreads().list(
       part='id, snippet',
       videoId=videoID,
-      #maxResults = 5
+      maxResults = 100
       )
    response = request.execute()
    comments_list.extend(process_comments(response['items']))
-
-   make_csv(comments_list, videoID)
-
    return comments_list
    #print(response.keys())
    # print(response)
@@ -66,7 +75,9 @@ def comment_threads(videoID):
 
 def main():
    #vid_id = input("Enter video ID: ")
-   comment_threads("2kyS6SvSYSE")
-
+   videoID = "2kyS6SvSYSE"
+   # make_csv(comment_threads(videoID))
+   videoIDs = ["puqaWrEC7tY", "d380meD0W0M", "gHZ1Qz0KiKM", "39idVpFF7NQ", "nc99ccSXST0"]
+   make_csv(video_threads(videoIDs))
 if __name__ == "__main__":
    main()
