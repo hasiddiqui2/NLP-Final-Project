78c0c5cc6a940ec798ed3b722a73823673765a40
irisshakya <46196492+irisshakya@users.noreply.github.com>
2023-05-14 16:21:53 -0400

    edit for csv reader
    will place the data in list of list, so multiple commas wont crash the reader

--- auth.py
+++ auth.py
@@ -1,22 +1,56 @@
 import pandas as pd
 import csv
 
+with open('/Users/iris/Documents/Hunter/SPRING_2023/nlp/final_proj/data/UScomments.csv', mode='r', encoding='utf-8-sig') as infile:
+    reader = csv.reader(infile, quoting=csv.QUOTE_ALL)
+    header_data = list(next(reader))
+    infile = infile.read().replace('\0', '').splitlines()
+    reader2 = csv.reader(infile)
+    reader3 = list(reader2)
+print(reader3[:3])
 
-try:
-   # to read count csv, should maintain usecols becasue commas are inside string, otherwise will crash
+# try:
+#     with open('/Users/iris/Documents/Hunter/SPRING_2023/nlp/final_proj/data/UScomments.csv', 'rb') as infile:
+#       data = infile.read()
+#       data = data.replace(b'\x00', b'')
+
+#     with open('file_clean.csv', 'wb') as outfile:
+#       outfile.write(data)
+
+#     with open('file_clean.csv', 'r') as csvfile:
+#       csvreader = csv.reader(csvfile)
+#       for row in csvreader:
+#         # Do something with the rows
+#         # f_data = pd.DataFrame(row[[1:]], columns=row[[0]])
+#         comment_data = pd.DataFrame(row, columns=['video_id', 'comment_text', 'likes', 'replies'])
+
+#       print(comment_data.tail())
+#         #print(comment_data.columns)
+
+
+# except Exception as e:
+#   print('ERROR in csv read')
+
+# #print(count_data[:5])
+
+# to read count csv, should maintain usecols becasue commas are inside string, otherwise will crash
    # i included to_csv to save the clipped data that works for us
-   count_data = pd.read_csv('/Users/iris/Documents/Hunter/SPRING_2023/nlp/final_proj/data/USvideos.csv', usecols=['video_id','title','channel_title','views','likes','dislikes','comment_total'])
+   #count_data = pd.read_csv('/Users/iris/Documents/Hunter/SPRING_2023/nlp/final_proj/data/USvideos.csv', usecols=['video_id','title','channel_title','views','likes','dislikes','comment_total'])
    
    # to open comment data
    # this returns an onject, so print() wont work
-   with open('/Users/iris/Documents/Hunter/SPRING_2023/nlp/final_proj/data/UScomments.csv', 'r') as f:
-      comments_data = csv.reader(f)
 
-except Exception as e:
-    print('error in the read_csv')
+# try:
+#   with open('/Users/iris/Documents/Hunter/SPRING_2023/nlp/final_proj/data/UScomments.csv', 'r') as f:
+#     comments_data = csv.reader(f)
+#     rows = [row for row in comments_data]
+#     # Convert the list of rows to a pandas DataFrame
+#     data = pd.DataFrame(rows[1:], columns=rows[0])
+
+# except Exception as e:
+#     print('ERROR IN THE read_csv')
 
 
-print(count_data[:5])
-print(comments_data)
-
-#count_data.to_csv('clean_vid.csv', index=False)
+    # Convert the list of rows to a pandas DataFrame
+#     data = pd.DataFrame(rows[1:], columns=rows[0])
+# print(comments_data.head())
--- public.py
+++ public.py
@@ -1,15 +1,7 @@
 import pandas as pd
-<<<<<<< HEAD
-=======
-# import numpy as n
-# import text2emotion as te
-# from dotenv import load_dotenv
->>>>>>> c3d1b4dcf685d83076cfc8e768dc4dc50e1da86f
 from googleapiclient.discovery import build
-# from iteration_utilities import unique_everseen
 from utils.comments import process_comments, make_csv
 
-<<<<<<< HEAD
 API_KEY = "AIzaSyDSLbJActSbVC0x97sbo_dpxfkP7dYCqB0"
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
@@ -37,64 +29,10 @@ def video_threads(videoIDs):
 def main():
   
    # extracting all video ids from dataset
-=======
-# API_KEY = "AIzaSyA5pIGUOIuLyFgf2BK7MCyiOFXmIUg9Gl8"
-API_KEY = "AIzaSyDSLbJActSbVC0x97sbo_dpxfkP7dYCqB0"
-youtube = build("youtube", "v3", developerKey=API_KEY)
-
-
-# def video_threads(videoIDs):
-#     comments_list = []
-#     for videoID in videoIDs:
-#         request = youtube.commentThreads().list(
-#             part='id, snippet',
-#             videoId=videoID,
-#             maxResults=100
-#         )
-#         response = request.execute()
-#         comments_list.extend(process_comments(response['items']))
-#     return comments_list
-
-
-def video_threads(videoIDs):
-    comments_list = []
-    inaccessible_vid_ids = []
-    for videoID in videoIDs:
-        try:
-            request = youtube.commentThreads().list(
-                part='id, snippet',
-                videoId=videoID,
-                maxResults=100
-            )
-            response = request.execute()
-            comments_list.extend(process_comments(response['items']))
-        except Exception as e:
-            # print(f"Error processing video ID {videoID}: {e}")
-            inaccessible_vid_ids.append(videoID)
-            continue
-    print('the following vids could not be accessed:')
-    print(inaccessible_vid_ids)
-    return comments_list
-    # print(response.keys())
-    # print(response)
-    # print(comments_list)
-
-
-def main():
-    # vid_id = input("Enter video ID: ")
-    videoID = "2kyS6SvSYSE"
-    # make_csv(comment_threads(videoID))
-    # you just need to replace the following array with an array that contains all the video id's from the database.
-    videoIDs = ["puqaWrEC7tY", "d380meD0W0M",
-                "gHZ1Qz0KiKM", "39idVpFF7NQ", "nc99ccSXST0"]
-
-    # extracting all video ids from dataset
->>>>>>> c3d1b4dcf685d83076cfc8e768dc4dc50e1da86f
     us_url = 'https://raw.githubusercontent.com/MayurDeshmukh10/youtube_analysis/master/USvideos.csv'
     us_data = pd.read_csv(us_url)
     video_ids = us_data['video_id'].tolist()
 
-<<<<<<< HEAD
     print('video ids:')
     print(len(video_ids))
     unique_video_ids = list(set(video_ids))
@@ -103,19 +41,5 @@ def main():
     print(len(unique_video_ids))
     make_csv(video_threads(unique_video_ids))
     
-=======
-    # print(video_ids)
-    print('video ids:')
-    print(len(video_ids))
-    unique_video_ids = list(set(video_ids))
-    # print(unique_video_ids)
-    print('unique video ids:')
-    print(len(unique_video_ids))
-
-    # make_csv(video_threads(videoIDs))
-    make_csv(video_threads(unique_video_ids))
-
-
->>>>>>> c3d1b4dcf685d83076cfc8e768dc4dc50e1da86f
 if __name__ == "__main__":
     main()
