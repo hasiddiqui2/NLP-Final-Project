b37f1ad7ef76e43a5190432bd32db51f19cc78bf
irisshakya <46196492+irisshakya@users.noreply.github.com>
2023-05-16 00:31:20 -0400

    old data vs new data

--- ml_model.py
+++ ml_model.py
@@ -15,13 +15,24 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 
+us_url = 'https://raw.githubusercontent.com/MayurDeshmukh10/youtube_analysis/master/USvideos.csv'
+us_data = pd.read_csv(us_url, usecols=['video_id', 'views', 'likes', 'dislikes','comment_count'], index_col=None)
 
-df = pd.read_csv('merged.csv', index_col=False)
-train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
-X_train = train_data[['views', 'likes','comment_total','compound_score']]
+our_data = pd.read_csv('merged.csv', index_col=False)
+
+#toggle comment between train_test to use our_data and,or us_data
+
+train_data, test_data = train_test_split(us_data, test_size=0.2, random_state=42)
+X_train = train_data[['views', 'likes','comment_count']]
 y_train = train_data["dislikes"]
-X_test = test_data[["views", "likes",'comment_total','compound_score']]
+X_test = test_data[["views", "likes",'comment_count']]
 y_test = test_data["dislikes"]
+
+#train_data, test_data = train_test_split(our_data, test_size=0.2, random_state=42)
+# X_train = train_data[['views', 'likes','comment_total','compound_score']]
+# y_train = train_data["dislikes"]
+# X_test = test_data[["views", "likes",'comment_total','compound_score']]
+# y_test = test_data["dislikes"]
 
 #scaling the dataset and predicting with linear regression
 scaler = StandardScaler()
@@ -30,26 +41,25 @@ X_test_scaled = scaler.transform(X_test)
 LRmodel = LinearRegression()
 LRmodel.fit(X_train_scaled, y_train)
 lr_y_pred = LRmodel.predict(X_test_scaled)
-print('\n')
 print("Mean Squared Error for Linear Regression:", round(mean_squared_error(y_test, lr_y_pred), 3))
 print("Root Mean Squared Error for Linear Regression:", round(mean_squared_error(y_test, lr_y_pred, squared=False), 3))
 print("R-squared Score for Linear Regression:", round(r2_score(y_test, lr_y_pred), 3))
+print('\n')
 
 # random forest
 RFmodel = RandomForestRegressor()
 RFmodel.fit(X_train_scaled, y_train)
 rf_y_pred = RFmodel.predict(X_test_scaled)
-print('\n')
-
 print("Mean Squared Error for Random Forest:", round(mean_squared_error(y_test, rf_y_pred), 3))
 print("Root Mean Squared Error for Random Forest:", round(mean_squared_error(y_test, rf_y_pred, squared=False), 3))
 print("R-squared Score for Random Forest:", round(r2_score(y_test, rf_y_pred), 3))
+print('\n')
 
 # k-neighbour
 KNmodel = KNeighborsRegressor(n_neighbors=20)
 KNmodel.fit(X_train_scaled, y_train)
 kn_y_pred = KNmodel.predict(X_test_scaled)
-print('\n')
 print("Mean Squared Error for K-Neighbour:", mean_squared_error(y_test, kn_y_pred))
 print("Root Mean Squared Error for K-Neighbour:", mean_squared_error(y_test, kn_y_pred, squared=False))
-print("R-squared Score for K-Neighbour:", r2_score(y_test, kn_y_pred))
+print("R-squared Score for K-Neighbour:", round(r2_score(y_test, kn_y_pred),3))
+print('\n')
